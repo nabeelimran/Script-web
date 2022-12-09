a102a1bbb21a0f075945a323d1476ce7aec4df3c
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-12-09 21:38:28 +0500

    tv changes pushed

--- src/components/SquareBox.js
+++ src/components/SquareBox.js
@@ -1,17 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const one = "bg-transparent border-2 border-primary";
 const two = "bg-primary";
 
 const variants = [one, two];
 
-const SquareBox = ({ children, variant = 0, className }) => {
+const SquareBox = ({ children, variant = 0, className, to = "/" }) => {
   return (
-    <div
+    <Link
+      to={to}
       className={`${variants[variant]} ${className} h-[70px] xl:h-[90px] py-4 px-5 rounded-lg justify-center flex flex-col items-center`}
     >
       {children}
-    </div>
+    </Link>
   );
 };
 
--- src/sections/TvHomepage/Channels.js
+++ src/sections/TvHomepage/Channels.js
@@ -156,37 +156,46 @@ function Channels() {
     <section>
       <div className="container">
         <div className="mb-16">
-          <div className="grid lg:grid-cols-[1fr_340px] gap-10 items-center mb-5 lg:mb-8">
+          <div className="grid lg:grid-cols-[1fr_340px] gap-10 items-center mb-5">
             <div className="flex flex-col md:flex-row items-center space-y-6 md:space-y-0 md:space-x-6 justify-between">
               <div className="w-full flex flex-col md:flex-row items-center space-y-6 md:space-y-0 md:space-x-6 flex-1">
-                <img
-                  src="images/tv/cultured-one.svg"
-                  className="max-w-[108px]"
-                  alt=""
-                />
-
-                <Button
-                  label="Follow"
-                  variant={4}
-                  customizationClassName="space-x-2 border-2 border-green px-5 rounded-lg text-green"
-                  LeftComponent={() => (
+                <div className="grid grid-cols-[110px_110px] gap-6">
+                  <div className="">
                     <img
-                      src="images/tv/green-heart.svg"
-                      className="w-[15px] lh-1"
+                      src="images/tv/cultured-one.svg"
+                      className="max-w-[108px] md:max-w-none md:w-full"
                       alt=""
                     />
-                  )}
-                />
+                  </div>
+
+                  <div className="">
+                    <Button
+                      label="Follow"
+                      variant={4}
+                      customizationClassName="space-x-2 border-2 border-green px-5 rounded-lg text-green justify-center flex w-full"
+                      LeftComponent={() => (
+                        <img
+                          src="images/tv/green-heart.svg"
+                          className="w-[15px] lh-1"
+                          alt=""
+                        />
+                      )}
+                    />
+                  </div>
+                </div>
 
                 <div className="flex-1 w-full">
-                  <div className="md:max-w-[300px] w-full">
-                    <FillBar barColor="#6C6C6C" bgColor="#1F1F1F" />
+                  <div className="md:max-w-[300px] w-full text-center md:text-left">
+                    {/* <FillBar barColor="#6C6C6C" bgColor="#1F1F1F" /> */}
+                    <p className="text-sm">Film Xyz</p>
+                    <p className="text-sm">this film abcabcabcabcabc</p>
                   </div>
                 </div>
               </div>
             </div>
             <div className="grid grid-cols-2 gap-4">
               <Button
+                link="/dashboard"
                 label="Dashboard"
                 variant={4}
                 customizationClassName="bg-primary px-4 rounded-lg text-black font-semibold space-x-2 justify-center"
@@ -195,6 +204,7 @@ function Channels() {
                 )}
               />
               <Button
+                link="/dashboard"
                 label="LitpoV6gf"
                 customizationClassName="bg-green text-black px-6 rounded-lg font-semibold justify-center"
                 variant={4}
@@ -204,7 +214,7 @@ function Channels() {
 
           <div className="grid xl:grid-cols-[1fr_340px] gap-10 items-center">
             <div className="xl:flex items-center space-y-12 xl:space-y-0 xl:space-x-6">
-              <div className="flex space-x-4 lg:space-x-6">
+              <div className="grid grid-cols-2 xl:grid-cols-[110px_110px] gap-4 xl:gap-6">
                 <SquareBox className="flex-1 xl:flex-auto" variant={1}>
                   <img
                     src="images/glasses.svg"
@@ -219,7 +229,7 @@ function Channels() {
                 <SquareBox className="flex-1 xl:flex-auto">
                   <img
                     src="images/tv/gem.svg"
-                    className="w-[26px] mb-2"
+                    className="w-[20px] mb-2"
                     alt=""
                   />
                   <div className="text-xs xl:text-sm bg-black font-medium lh-1_2 rounded text-center">
@@ -244,14 +254,18 @@ function Channels() {
             </div>
 
             <div className="grid grid-cols-2 gap-4">
-              <SquareBox className="flex-1 xl:flex-auto" variant={1}>
+              <SquareBox
+                to="/dashboard"
+                className="flex-1 xl:flex-auto"
+                variant={1}
+              >
                 <h1 className="fs-24px text-black font-semibold mb-1">4.75</h1>
                 <h1 className="text-xs xl:text-sm text-black font-medium text-center">
                   SPAY In WALLET
                 </h1>
               </SquareBox>
 
-              <SquareBox className="flex-1 xl:flex-auto">
+              <SquareBox to="/dashboard" className="flex-1 xl:flex-auto">
                 <h1 className="fs-24px text-primary font-semibold mb-1">203</h1>
                 <h1 className="text-xs xl:text-sm text-primary font-medium text-center">
                   Earned Today
