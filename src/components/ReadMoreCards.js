e1100adc7d606183cbeb6c0c2f462b59fe799160
Dheeraj <dheeraj.sharma@oodles.io>
2022-12-09 21:49:35 +0530

    blog api integration done

--- src/components/ReadMoreCards.js
+++ src/components/ReadMoreCards.js
@@ -1,18 +1,21 @@
 import React from "react";
 import Title from "./Title";
 
-function ReadMoreCards({ img = "images/read-more-banner.png" }) {
+function ReadMoreCards({ img = "images/read-more-banner.png", blog }) {
+  const openInNewTab = (url) => {
+    window.open(url, '_blank', 'noreferrer');
+  };
   return (
-    <div className="bg-grey-1 rounded-md overflow-hidden">
-      <img src={img} className="w-full block object-cover h-[159px]" alt="" />
+    <div className="bg-grey-1 rounded-md overflow-hidden" onClick={() => openInNewTab(blog.link)}>
+      <img src={blog.thumbnail} className="w-full block object-cover h-[159px]" alt="" />
 
       <div className="px-4 py-4 pb-8">
         <Title className="font-medium mb-3" variant="18">
-          Stargate Finance (STG)
+          {blog.title}
         </Title>
 
-        <p className="text-sm opacity-80">
-          An Omnichain Liquidity Transport Protocol.
+        <p className="text-sm opacity-80" >
+          {blog.author}
         </p>
       </div>
     </div>
