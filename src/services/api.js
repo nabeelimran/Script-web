4e2b770b8b2d00a10d1b7727f5656a417c99c929
Dheeraj <dheeraj.sharma@oodles.io>
2022-12-19 12:36:52 +0530

    logout function added

--- src/services/api.js
+++ src/services/api.js
@@ -170,4 +170,18 @@ export default class Api {
         }
         return axios.put(`${APIPATH.BASEURL}save/duration`, req, options);
     }
+
+    static logout(req, screenName) {
+        const options = {
+            headers:{
+                ipAddress: 'dummyData',
+                latitude: 'dummyData',
+                longitude: 'dummyData',
+                countryName: 'dummyData',
+                screenName: screenName,
+                Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkaGVlcmFqMjQxNEB5b3BtYWlsLmNvbSIsImF1dGgiOltdLCJyb2xlIjoiUk9MRV9VU0VSIiwiaWF0IjoxNjcxMTk4MjI1LCJleHAiOjE2NzM4MjgwMjV9.wxfULBdALmY9DGMqXaWKZ1B8w6r6ynyCAfuYjaS7Snw"
+            }
+        }
+        return axios.post(`${APIPATH.BASEURL}logout`,req, options);
+    }
 }
