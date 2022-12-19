cae8498b7f87df49c4cb73cf614fb004d5813c7d
Shikhar Gupta <shikhar.gupta@oodles.io>
2022-12-19 14:18:21 +0530

    active class in modal

--- src/components/GlassPopup.js
+++ src/components/GlassPopup.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import FillBar from "./FillBar";
 import Popup from "./Popup";
 
 function GlassPopup({ open, setOpen }) {
+  const [active, setActive] = useState(1);
+
+  const changeActiveState = (id) => {
+    setActive(id);
+  };
+
+  const returnClasses = (id) => {
+    if (id === active) {
+      return "active-class";
+    } else {
+      return "";
+    }
+  };
+
   return (
     <>
       {/* <Popup
@@ -36,8 +50,9 @@ function GlassPopup({ open, setOpen }) {
 
       <Popup
         open={open}
+        glass={true}
         setOpen={setOpen}
-        className="max-w-[400px] w-full bg-black text-white py-5 px-8 rounded-lg mx-auto shadow-primary shadow"
+        className="max-w-[400px] w-full bg-black text-white py-5 px-8 rounded-lg mx-auto border border-primary"
       >
         {/* <img
           src="images/tv/glasses.svg"
@@ -63,37 +78,65 @@ function GlassPopup({ open, setOpen }) {
         </div> */}
 
         <h2 className="text-center mb-3">Glasses</h2>
-
         <div className="max-h-48 overflow-auto">
-          <div className="flex py-2 mb-3 rounded w-11/12 mx-auto bg-[#131313] cursor-pointer">
+          <div
+            className={`flex py-2 mb-3 rounded w-11/12 mx-auto bg-[#131313] cursor-pointer ${returnClasses(
+              1
+            )}`}
+            onClick={() => changeActiveState(1)}
+          >
             <img src="images/tv/glasses.svg" alt="" className="mx-4" />
             <div>
               <h6>Hi</h6>
               <p className="text-xs">Hlo</p>
             </div>
           </div>
-          <div className="flex py-2 mb-3 rounded w-11/12 mx-auto bg-[#131313] cursor-pointer">
+
+          <div
+            className={`flex py-2 mb-3 rounded w-11/12 mx-auto bg-[#131313] cursor-pointer ${returnClasses(
+              2
+            )}`}
+            onClick={() => changeActiveState(2)}
+          >
             <img src="images/tv/glasses.svg" alt="" className="mx-4" />
             <div>
               <h6>Hi</h6>
               <p className="text-xs">Hlo</p>
             </div>
           </div>
-          <div className="flex py-2 mb-3 rounded w-11/12 mx-auto bg-[#131313] cursor-pointer">
+
+          <div
+            className={`flex py-2 mb-3 rounded w-11/12 mx-auto bg-[#131313] cursor-pointer ${returnClasses(
+              3
+            )}`}
+            onClick={() => changeActiveState(3)}
+          >
             <img src="images/tv/glasses.svg" alt="" className="mx-4" />
             <div>
               <h6>Hi</h6>
               <p className="text-xs">Hlo</p>
             </div>
           </div>
-          <div className="flex py-2 mb-3 rounded w-11/12 mx-auto bg-[#131313] cursor-pointer">
+
+          <div
+            className={`flex py-2 mb-3 rounded w-11/12 mx-auto bg-[#131313] cursor-pointer ${returnClasses(
+              4
+            )}`}
+            onClick={() => changeActiveState(4)}
+          >
             <img src="images/tv/glasses.svg" alt="" className="mx-4" />
             <div>
               <h6>Hi</h6>
               <p className="text-xs">Hlo</p>
             </div>
           </div>
-          <div className="flex py-2 mb-3 rounded w-11/12 mx-auto bg-[#131313] cursor-pointer">
+
+          <div
+            className={`flex py-2 mb-3 rounded w-11/12 mx-auto bg-[#131313] cursor-pointer ${returnClasses(
+              5
+            )}`}
+            onClick={() => changeActiveState(5)}
+          >
             <img src="images/tv/glasses.svg" alt="" className="mx-4" />
             <div>
               <h6>Hi</h6>
@@ -103,7 +146,12 @@ function GlassPopup({ open, setOpen }) {
         </div>
 
         <div className="text-center mt-5">
-          <button className="px-5 py-1 rounded bg-[#131313]">Click</button>
+          <button
+            className="px-5 py-1 rounded bg-[#131313]"
+            onClick={() => setOpen(false)}
+          >
+            Continue
+          </button>
         </div>
       </Popup>
     </>
