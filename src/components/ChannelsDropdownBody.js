7b803a2140bdefa78a3154320e78a83bf508818f
Dheeraj <dheeraj.sharma@oodles.io>
2022-12-22 19:16:44 +0530

    channel commented

--- src/components/ChannelsDropdownBody.js
+++ src/components/ChannelsDropdownBody.js
@@ -6,7 +6,7 @@ const channels = [
   { img: "/images/channels/cultured-one.svg", className: "w-full lg:w-20" },
   { img: "/images/channels/toom.svg", className: "w-full lg:w-20" },
   { img: "/images/channels/drama.svg", className: "w-full lg:w-auto" },
-  { img: "/images/channels/cultured-two.svg", className: "w-full lg:w-20" },
+  // { img: "/images/channels/cultured-two.svg", className: "w-full lg:w-20" },
   { img: "/images/channels/film.svg", className: "w-full lg:w-auto" },
   { img: "/images/channels/irl.svg", className: "w-full lg:w-14" },
 ];
@@ -28,21 +28,23 @@ const ChannelBox = ({ img, label, to = "/" }) => {
   );
 };
 
-const ChannelsDropdownBody = ({ channels }) => {
-  if (channels && channels.length > 0) {
-    channels = channels.map((channel) => {
-      if (channel.id === 1922 || channel.id === 1913) {
-        channel.className = "w-full lg:w-20";
-      }
-      if (channel.id === 1911 || channel.id === 1908) {
-        channel.className = "w-full lg:w-auto";
-      }
-      if (channel.id === 621730) {
-        channel.className = "w-full lg:w-14";
-      }
-      return channel;
-    });
-  }
+const ChannelsDropdownBody = (
+  // { channels }
+  ) => {
+  // if (channels && channels.length > 0) {
+  //   channels = channels.map((channel) => {
+  //     if (channel.id === 1922 || channel.id === 1913) {
+  //       channel.className = "w-full lg:w-20";
+  //     }
+  //     if (channel.id === 1911 || channel.id === 1908) {
+  //       channel.className = "w-full lg:w-auto";
+  //     }
+  //     if (channel.id === 621730) {
+  //       channel.className = "w-full lg:w-14";
+  //     }
+  //     return channel;
+  //   });
+  // }
 
   return (
     <div className="bg-blue-3 rounded-lg grid lg:grid-cols-[230px,1fr] lg:p-0 gap-6 lg:gap-0 py-4 lg:py-0">
@@ -67,7 +69,7 @@ const ChannelsDropdownBody = ({ channels }) => {
           {channels && channels.length > 0
             ? channels.map((item, i) => (
                 <img
-                  src={item.image}
+                  src={item.img}
                   key={i}
                   alt="abc"
                   className={`lg:mx-3 lg:my-3 ${item.className}`}
