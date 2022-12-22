6df7400958e26e3f0ae8b2bdbe1d2f192bec5f14
Dheeraj <dheeraj.sharma@oodles.io>
2022-12-22 11:17:51 +0530

    if condition added

--- src/components/ChannelsDropdownBody.js
+++ src/components/ChannelsDropdownBody.js
@@ -30,19 +30,21 @@ const ChannelBox = ({ img, label, to = "/" }) => {
 const ChannelsDropdownBody = ({
   channels
 }) => {
-  console.log(channels, 'asdas');
-  channels = channels.map((channel) => {
-    if(channel.id === 1922 || channel.id === 1913) {
-      channel.className = "w-full lg:w-20";
-    }
-    if(channel.id === 1911 || channel.id === 1908) {
-      channel.className = "w-full lg:w-auto";
-    }
-    if(channel.id === 621730) {
-      channel.className = "w-full lg:w-14";
-    }
-    return channel;
-  })
+  if(channels && channels.length > 0) {
+    channels = channels.map((channel) => {
+      if(channel.id === 1922 || channel.id === 1913) {
+        channel.className = "w-full lg:w-20";
+      }
+      if(channel.id === 1911 || channel.id === 1908) {
+        channel.className = "w-full lg:w-auto";
+      }
+      if(channel.id === 621730) {
+        channel.className = "w-full lg:w-14";
+      }
+      return channel;
+    })
+  }
+  
   return (
     <div className="bg-blue-3 rounded-lg grid lg:grid-cols-[230px,1fr] lg:p-0 gap-6 lg:gap-0 py-4 lg:py-0">
       <div className="lg:py-7 px-5 lg:px-8 flex-col space-y-3 lg:space-y-5">
